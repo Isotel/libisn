611903f1e0390dcd7568bef0d52eab4b0031356d
Uros Platise <uros@isotel.eu>
2019-01-09 11:03:09 +0100

    Moved C sources under c/ to be ready for further languages.

--- c/include/isn.h
+++ c/include/isn.h
@@ -1,5 +1,23 @@
 /** \file
+ *  \author Uros Platise <uros@isotel.eu>
  *  \see https://www.isotel.eu/isn
+ * 
+ * (c) Copyright 2019, Isotel, http://isotel.eu
+ * 
+ * This file is part of Isotel ISN library named libisn.
+ * 
+ * libisn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version.
+ * 
+ * libisn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with libisn. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef __ISN_H__
