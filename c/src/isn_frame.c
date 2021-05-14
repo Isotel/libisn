fee1d784e0db96ad42dc5b8aaed662cd3005753f
tomaz <tomaz@isotel.eu>
2021-05-14 10:48:51 +0200

    Merge

--- c/src/isn_frame.c
+++ c/src/isn_frame.c
@@ -15,6 +15,12 @@
  */
 
 #include "isn_frame.h"
+#include "debug.h"
+
+//
+isn_layer_t *trigger_layer;
+
+//
 
 /**\{ */
 
@@ -55,7 +61,6 @@ static void isn_frame_free(isn_layer_t *drv, const void *ptr) {
     const uint8_t *buf = ptr;
     if (buf) obj->parent->free(obj->parent, buf - 1);
 }
-
 
 static int isn_frame_send(isn_layer_t *drv, void *dest, size_t size) {
     isn_frame_t *obj = (isn_frame_t *)drv;
